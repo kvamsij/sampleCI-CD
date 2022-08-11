23f82630df0a6167bacbaaa22d1e57227e8dee7b
Vamsi Kalpamanu <kvamsij@gmail.com>
2022-08-11 19:19:21 +0100

    testing for sample branch
    Signed-off-by: Vamsi Kalpamanu <kvamsij@gmail.com>

--- dist/index.js
+++ dist/index.js
@@ -1,19 +1,17 @@
-(() => {
-  var t = (r, e) => () => (e || r((e = { exports: {} }).exports, e), e.exports);
-  var s = t((b, n) => {
-    var i = (r, e) => {
-        if (!Number.isInteger(r) || !Number.isInteger(e)) throw new Error('input values must be an integer');
-        return r + e;
-      },
-      o = (r, e) => {
-        if (!Number.isInteger(r) || !Number.isInteger(e)) throw new Error('input values must be an integer');
-        return (r + e) / 2;
-      };
-    n.exports = { sum: i, average: o };
-  });
-  var m = t((p, u) => {
-    var g = s();
-    u.exports = g;
-  });
-  m();
-})();
+var t = (r, e) => () => (e || r((e = { exports: {} }).exports, e), e.exports);
+var s = t((b, n) => {
+  var i = (r, e) => {
+      if (!Number.isInteger(r) || !Number.isInteger(e)) throw new Error('input values must be an integer');
+      return r + e;
+    },
+    o = (r, e) => {
+      if (!Number.isInteger(r) || !Number.isInteger(e)) throw new Error('input values must be an integer');
+      return (r + e) / 2;
+    };
+  n.exports = { sum: i, average: o };
+});
+var m = t((p, u) => {
+  var g = s();
+  u.exports = g;
+});
+export default m();
