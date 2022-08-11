573b4d2080d4762d91166c8284173b8c24f8dc1e
Vamsi Kalpamanu <kvamsij@gmail.com>
2022-08-11 16:12:44 +0100

    add esbuild
    Signed-off-by: Vamsi Kalpamanu <kvamsij@gmail.com>

---
+++ __test__/average.spec.js
@@ -0,0 +1,15 @@
+const { average } = require('../libs');
+const result = average(1, 5);
+
+describe('AVERAGE', () => {
+  it('should return number', () => {
+    expect(Number.isInteger(result)).toBeTruthy();
+  });
+  it('should return 3 when passed 1, 5 as arguments', () => {
+    expect(result).toEqual(3);
+  });
+
+  it('should must throw an error if the inputs not a number', () => {
+    expect(() => average('a', 'b')).toThrowError('input values must be an integer');
+  });
+});
